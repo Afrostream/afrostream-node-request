07c0a8376f166b0a10265c675a53920a3a4b83ad
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-09-29 15:57:13 +0200

    filters: new filter 200OKNotEmpty.js

---
+++ filters/200OKNotEmpty.js
@@ -0,0 +1,14 @@
+var log = require('../log.js');
+
+module.exports = function (data) {
+  var response = data[0]
+    , body = data[1];
+
+  if (response.statusCode !== 200 || !body) {
+    log.error(this, response.statusCode + ' ' + JSON.stringify(body));
+    var error = new Error(body && body.error || 'unknown');
+    error.statusCode = response.statusCode || 500;
+    throw error;
+  }
+  return data;
+};
