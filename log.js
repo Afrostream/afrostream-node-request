951b3310410edff7631d78eeed3f95fc536842eb
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-09-30 17:59:26 +0200

    bugfix: missing log.js

---
+++ log.js
@@ -0,0 +1,35 @@
+var log = function () {
+  var args = Array.prototype.slice.call(arguments);
+  var level = args.shift();
+  var options = args.shift();
+
+  args.unshift('[' + level + ']: [' + options.name + ']: [REQUEST-'+ (options.requestId || '?') +']: ');
+  switch (level) {
+    case 'ERROR':
+      console.error.apply(console, args);
+      break;
+    case 'DEBUG':
+    case 'INFO':
+    default:
+      console.log.apply(console, args);
+      break;
+  }
+};
+
+module.exports = {
+  debug: function () {
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift('DEBUG');
+    log.apply(null, args);
+  },
+  info:function () {
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift('INFO');
+    log.apply(null, args);
+  },
+  error:function () {
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift('ERROR');
+    log.apply(null, args);
+  }
+};
